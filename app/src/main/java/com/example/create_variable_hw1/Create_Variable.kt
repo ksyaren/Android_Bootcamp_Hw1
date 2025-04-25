a69343b6e6c71e69364071d73a5e6da7ada3488f
Yaren Kose <ksyarenn@gmail.com>
2025-04-26 00:16:58 +0300

    Final

---
+++ app/src/main/java/com/example/create_variable_hw1/Create_Variable.kt
@@ -0,0 +1,56 @@
+package com.example.create_variable_hw1
+
+fun main() {
+
+    val city = "Ankara"
+    val country = "Turkey"
+    val phone = "0555 555 555"
+    val postal_code = 77100
+    val email = "john@gmail.com"
+    val job = "Student"
+    val stock_quantity = 5
+    val customer_name = "John"
+    val balance = 60
+    val birthday = "14 April 2003"
+    val salary = 5000
+    val marital_status = true
+    val purchase_comment = "Good product"
+    val payment_date = "01 June 2025"
+    val payment = 120
+    val order_quantity = 2
+    val car_model = "Toyota Corolla"
+    val book_title = "The Alchemist"
+    val publication_date = "1988"
+    val discount_amount = 10
+    val room_count = 3
+    val latitude = 39.9208
+    val longitude = 32.8541
+    val product_name = "Laptop"
+    val food_price = 250
+    val brand = "Apple"
+    val music_title = "Bohemian Rhapsody"
+    val video_duration = "2:30"
+    val product_rating = 4.5
+    val image_name = "sunset.jpg"
+    val file_format = "jpg"
+    val color = "Red"
+    val color_code = "#FF0000"
+    val phone_model = "iPhone 14"
+    val screen_size = 6.1
+    val weight = 180
+    val national_day = "29 October"
+    val holiday = "New Year's Day"
+    val reservation_date = "20 December 2025"
+    val street_name = "Main Street"
+    val bus_line = "34A"
+    val remaining_minutes = 15
+    val tracking_code = "ABC123XYZ"
+    val coupon_duration = 30
+    val coupon_code = "DISCOUNT30"
+    val invoice_address = "123 Main Street, City, Country"
+
+
+
+
+
+}
